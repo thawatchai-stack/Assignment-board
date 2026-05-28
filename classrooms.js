6e2ce3f0dc6329919906ba4dadb9cddc714a32f4
thawatchai-stack <thawatchai@tkk.ac.th>
2026-05-28 12:24:48 +0700

    Add files via upload

---
+++ classrooms.js
@@ -0,0 +1,14 @@
+const CLASSROOMS = [
+  "ปวช.1/1",
+  "ปวช.1/2",
+  "ปวช.1/3",
+  "ปวช.1/4",
+  "ปวช.2/1",
+  "ปวช.2/2",
+  "ปวช.2/3",
+  "ปวช.2/4"
+];
+
+if (typeof window !== 'undefined') {
+  window.CLASSROOMS = CLASSROOMS;
+}
